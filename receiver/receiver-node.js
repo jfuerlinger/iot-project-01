593748eb915d0fd996f3a412379f9e4e1a5a9af3
Josef Fürlinger <josef.fuerlinger@gmail.com>
2016-11-18 20:35:38 +0100

    nodejs-Implementierung hinzugefügt.

---
+++ receiver/receiver-node.js
@@ -0,0 +1,41 @@
+// PubNub = require('pubnub');      ES5
+import PubNub from 'pubnub';        ES6
+ 
+var pubnub = new PubNub({
+    subscribeKey: "sub-c-59ea358c-ace2-11e6-b37b-02ee2ddab7fe",
+    publishKey: "pub-c-aec2c9d2-5afc-4677-993a-27da78960aec",
+    ssl: true
+});
+
+pubnub.addListener({
+    
+    message: function(m) {
+        // handle message
+        var channelName = m.channel; // The channel for which the message belongs
+        var channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+        var pubTT = m.timetoken; // Publish timetoken
+        var msg = m.message; // The Payload
+
+        console.log(msg);
+    },
+    presence: function(p) {
+        // handle presence
+        var action = p.action; // Can be join, leave, state-change or timeout
+        var channelName = p.channel; // The channel for which the message belongs
+        var occupancy = p.occupancy; // No. of users connected with the channel
+        var state = p.state; // User State
+        var channelGroup = p.subscription; //  The channel group or wildcard subscription match (if exists)
+        var publishTime = p.timestamp; // Publish timetoken
+        var timetoken = p.timetoken;  // Current timetoken
+        var uuid = p.uuid; // UUIDs of users who are connected with the channel
+    },
+    status: function(s) {
+        // handle status
+    }
+});
+
+pubnub.subscribe({
+    channels: ["hello_world"],
+    withPresence: true // also subscribe to presence instances.
+})
+
