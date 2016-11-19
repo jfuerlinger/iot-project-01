d395d0b8e00f5656385178ef20981b1f98d3b5ed
Josef Fürlinger <josef.fuerlinger@gmail.com>
2016-11-19 12:39:27 +0100

    GPIO-Pin geändert.

--- receiver/receiver-node.js
+++ receiver/receiver-node.js
@@ -2,7 +2,7 @@ PubNub = require("pubnub");
 var gpio = require("pi-gpio");
 
 var channelName = "IoTChannel";
-var gpioLedPin = 25;
+var gpioLedPin = 7;
 
 var pubnub = new PubNub({
     subscribeKey: "sub-c-59ea358c-ace2-11e6-b37b-02ee2ddab7fe",
@@ -32,19 +32,15 @@ pubnub.addListener({
                 break;
             case "enableLed":
                 console.log("Enabling the LED ...");
-                gpio.open(gpioLedPin, "output", function (err) { // Open pin 16 for output
-                    gpio.write(gpioLedPin, 1, function () { // Set pin 16 high (1)
-                        gpio.close(gpioLedPin); // Close pin 16
-                    });
+                gpio.open(gpioLedPin, "output", function (err) { 
+                    gpio.write(gpioLedPin, 1);
                 });
                 console.log("Done.");
                 break;
             case "disableLed":
                 console.log("Disabling the LED ...");
-                gpio.open(gpioLedPin, "output", function (err) { // Open pin 16 for output
-                    gpio.write(gpioLedPin, 0, function () { // Set pin 16 high (1)
-                        gpio.close(gpioLedPin); // Close pin 16
-                    });
+                gpio.open(gpioLedPin, "output", function (err) { 
+                    gpio.write(gpioLedPin, 0);
                 });
                 console.log("Done.");
                 break;
