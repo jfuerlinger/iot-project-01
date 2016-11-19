563ac6ffa91efdff92a8308fd54918747c455256
Josef Fürlinger <josef.fuerlinger@gmail.com>
2016-11-19 12:13:53 +0100

    GPIO-Logik hinzugefügt.

--- receiver/receiver-node.js
+++ receiver/receiver-node.js
@@ -1,11 +1,17 @@
-PubNub = require("pubnub"); //ES5
-//import PubNub from 'pubnub';        //ES6
+PubNub = require("pubnub"); 
+var gpio = require("pi-gpio");
+
 var channelName = "IoTChannel";
+var gpioLedPin = 25;
+
 var pubnub = new PubNub({
     subscribeKey: "sub-c-59ea358c-ace2-11e6-b37b-02ee2ddab7fe",
     publishKey: "pub-c-aec2c9d2-5afc-4677-993a-27da78960aec",
     ssl: true
 });
+
+
+
 
 pubnub.addListener({
 
@@ -26,9 +32,21 @@ pubnub.addListener({
                 break;
             case "enableLed":
                 console.log("Enabling the LED ...");
+                gpio.open(gpioLedPin, "output", function (err) { // Open pin 16 for output
+                    gpio.write(gpioLedPin, 1, function () { // Set pin 16 high (1)
+                        gpio.close(gpioLedPin); // Close pin 16
+                    });
+                });
+                console.log("Done.");
                 break;
             case "disableLed":
                 console.log("Disabling the LED ...");
+                gpio.open(gpioLedPin, "output", function (err) { // Open pin 16 for output
+                    gpio.write(gpioLedPin, 0, function () { // Set pin 16 high (1)
+                        gpio.close(gpioLedPin); // Close pin 16
+                    });
+                });
+                console.log("Done.");
                 break;
         }
 
