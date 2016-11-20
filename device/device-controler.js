cfa3e4265d44579db88fa2a8c530ec2a839a13ee
Josef Fürlinger <josef.fuerlinger@gmail.com>
2016-11-20 11:24:30 +0100

    sudo hinzugefügt.

--- device/device-controler.js
+++ device/device-controler.js
@@ -56,7 +56,7 @@ pubnub.addListener({
                 break;
 
             case "getMeasurementRequest":
-                var cmd = "./device/tools/sht21 S";
+                var cmd = "sudo ./device/tools/sht21 S";
 
                 var sht21Process = exec(cmd, function (error, stdout, stderr) {
                     //if (stdout)
